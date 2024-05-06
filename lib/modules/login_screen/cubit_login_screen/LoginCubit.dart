3eebbe707fa1f95ad381456a3036e9b789c08f70
Your Name <you@example.com>
2024-05-06 17:26:53 +0300

    frist commit

--- lib/modules/login_screen/cubit_login_screen/LoginCubit.dart
+++ lib/modules/login_screen/cubit_login_screen/LoginCubit.dart
@@ -1,4 +1,5 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,14 +10,37 @@ class HomelyLoginCubit extends Cubit<HomelyLoginStates>
   HomelyLoginCubit():super(HomelyLoginInitialState());
 
   static HomelyLoginCubit get(context)=>BlocProvider.of(context);
- IconData suffix= Icons.visibility_outlined;
- bool isPassword=true;
-void ChangePassword(){
-  isPassword=! isPassword;
-  suffix=isPassword?Icons.visibility_outlined:Icons.visibility_off_outlined;
-  emit(HomelyLoginLoadingState());
-}
 
+  void userLogin({
+    required String email,
+    required String password
+  })
+  {
+    emit(HomelyLoginLoadingState());
+    FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password
+    ).then((value){
+      emit(HomelyLoginSuccessState());
 
+    }).catchError((error){
+      emit(HomelyLoginErrorState(error.toString()));
+    });
   }
+
+
+
+
+
+
+  IconData suffix= Icons.visibility_outlined;
+  bool isPassword=true;
+  void ChangePassword(){
+    isPassword=! isPassword;
+    suffix=isPassword?Icons.visibility_outlined:Icons.visibility_off_outlined;
+    emit(HomelyLoginChangePasswordState());
+  }
+
+
+}
 
--- lib/modules/login_screen/login_screen.dart
+++ lib/modules/login_screen/login_screen.dart
@@ -1,8 +1,12 @@
+import 'package:apphomely/modules/login_screen/cubit_login_screen/LoginCubit.dart';
+import 'package:apphomely/modules/login_screen/cubit_login_screen/States_login_screen/LoginStates.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import '../../layout/homeApp.dart';
 import '../../layout/home_screen.dart';
@@ -23,23 +27,23 @@ class _LoginScreenState extends State<LoginScreen> {
   Future signInWithGoogle() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-       if(googleUser == null){
-         return;
-       }
+    if(googleUser == null){
+      return;
+    }
     // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
     );
 
     // Once signed in, return the UserCredential
-     await FirebaseAuth.instance.signInWithCredential(credential);
-     Navigator.pushReplacement(context,
+    await FirebaseAuth.instance.signInWithCredential(credential);
+    Navigator.pushReplacement(context,
         MaterialPageRoute(
-            builder: (context) => Homely_Home())
+            builder: (context) => HomeScreen())
     );
   }
   var formKey=GlobalKey<FormState>();
@@ -48,140 +52,182 @@ class _LoginScreenState extends State<LoginScreen> {
   bool passShow=true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('تسجيل دخول',
-          style:TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold
-          ) ,
-        ),
-        centerTitle: true,
-        actions: [],
-      ),
-      body:Center(
-        child: ListView(
-          children: [
-            Form(
-              key:formKey ,
-              child:  Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    defaultTextFormField(
-                      controller: UserController,
-                      type: TextInputType.emailAddress,
-                      validator: (value){
-                        if (value!.isEmpty){
-                          return'اسم المستخدم غير صحيح';
-                        }
-                        return null;
-                      },
-                      label: 'اسم المستخدم',
-                      prefixIcon: Icons.person_2_outlined,
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    defaultTextFormField(
-                        controller: passwordController,
-                        type: TextInputType.text,
-                        validator: (value){
-                          if (value!.isEmpty){
-                            return'كلمة المرور غير صحيحه';
-                          }
-                        },
-                        label: 'كلمة المرور',
-                        prefixIcon: Icons.lock_outline,
-                        suffix:passShow?Icons.visibility_off_outlined:Icons.visibility_outlined,
-                        obscureText: passShow,
-                        suffixPressed: () {
-                          setState(() {
-                            passShow = !passShow;
-                          });
-                        }),
-                    SizedBox(
-                      height: 0.0,
-                    ),
-                    TextButton(onPressed: (){
-
-                    }, child:Text(
-                        'هل نسيت كلمة المرور',
-                      style: TextStyle(
-                        fontSize: 10.0,
-                        color: Colors.grey[500]
-                      ),
-                    )
-                    ),
-                    SizedBox(height: 10.0,),
-                    defaultButton(
-                        background: Colors.deepOrangeAccent,
-                        function: ()async {
-                          if (formKey.currentState!.validate()) {
-                            try {
-                              final credential = await FirebaseAuth.instance
-                                  .signInWithEmailAndPassword(
-                                  email: UserController.text,
-                                  password: passwordController.text
-                              );
-                              Navigator.pushReplacement(context,
-                                  MaterialPageRoute(
-                                      builder: (context) => HomeScreen())
-                              );
-                            } on FirebaseAuthException catch (e) {
-                              if (e.code == 'user-not-found') {
-                                print(
-                                    '=====لم يتم العثور علي مستخدم لهذ الايميل======');
-                                AwesomeDialog(
-                                  context: context,
-                                  dialogType: DialogType.error,
-                                  animType: AnimType.rightSlide,
-                                  title: ' خطاء',
-                                  desc: 'لم يتم العثور علي اسم مستخدم',
-                                ).show();
-                              } else if (e.code == 'wrong-password') {
-                                print('======كلمه المرور غير صحيحه=======');
-                                AwesomeDialog(
-                                  context: context,
-                                  dialogType: DialogType.error,
-                                  animType: AnimType.rightSlide,
-                                  title: ' خطاء',
-                                  desc: 'كلمه المرور غير صحيحه',
-                                ).show();
-                              }
-                            }
-                          }
-                        } ,
-                        text: 'تسجيل دخول'
-
-                    ),
-                    SizedBox(height: 20.0,),
-                    defaultButton(
-                        function: (){
-                          signInWithGoogle();
-                        }, text: 'تسجيل دخول جوجل'
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(onPressed: (){
-                          navigatorTo(context, register());
-                        }, child: Text('انشاء حساب'),
-                        ),
-                      ],
-                    )
-
-                  ],
-                ),
-              ),
+    return BlocProvider(
+      create: (BuildContext context)=>HomelyLoginCubit(),
+      child:BlocConsumer<HomelyLoginCubit,HomelyLoginStates>(
+        listener: ( context,  state) {
+          if(state is HomelyLoginErrorState){
+            showToast(
+                text: state.error,
+                state: toastState.error
+            );
+            // AwesomeDialog(
+            //   context: context,
+            //   dialogType: DialogType.error,
+            //   animType: AnimType.rightSlide,
+            //   title: ' خطاء',
+            //   desc: 'لم يتم العثور علي المستخدم',
+            // ).show();
+          }
+          if (state is HomelyLoginSuccessState) {
+            pushAndRemoveUntil(context, HomeScreen());
+          }
+        },
+        builder: (BuildContext context, state) { return Scaffold(
+          appBar: AppBar(
+            title: Text('تسجيل دخول',
+              style:TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold
+              ) ,
             ),
-          ],
-        ),
-      ) ,
+            centerTitle: true,
+            actions: [],
+          ),
+          body:Center(
+            child: ListView(
+              children: [
+                Form(
+                  key:formKey ,
+                  child:  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        defaultTextFormField(
+                          controller: UserController,
+                          type: TextInputType.emailAddress,
+                          validator: (value){
+                            if (value!.isEmpty){
+                              return'اسم المستخدم غير صحيح';
+                            }
+                            return null;
+                          },
+                          label: 'اسم المستخدم',
+                          prefixIcon: Icons.person_2_outlined,
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        defaultTextFormField(
+                            controller: passwordController,
+                            type: TextInputType.text,
+                            validator: (value){
+                              if (value!.isEmpty){
+                                return'كلمة المرور غير صحيحه';
+                              }
+                              return null;
+                            },
+                            label: 'كلمة المرور',
+                            prefixIcon: Icons.lock_outline,
+                            suffix:passShow?Icons.visibility_off_outlined:Icons.visibility_outlined,
+                            obscureText: passShow,
+                            suffixPressed: () {
+                              setState(() {
+                                passShow = !passShow;
+                              });
+                            }),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        SizedBox(height: 10.0,),
+                        ConditionalBuilder(
+                            condition: state is! HomelyLoginLoadingState,
+                            builder: (context)=>Center(
+                              child: defaultButton(
+                                  width:150.0,
+                                  function: (){
+                                    if(formKey.currentState!.validate() )
+                                    {
+                                      HomelyLoginCubit.get(context).userLogin(
+                                          email:UserController.text ,
+                                          password: passwordController.text
+                                      );
+
+                                    }
+                                  },
+                                  text: 'تسجيل دخول'),
+                            ),
+                            fallback: (context)=>Center(child: CircularProgressIndicator(),)
+                        ),
+                        // defaultButton(
+                        //     background: Colors.deepOrangeAccent,
+                        //     function: ()async {
+                        //       if (formKey.currentState!.validate()) {
+                        //         try {
+                        //           final credential = await FirebaseAuth.instance
+                        //               .signInWithEmailAndPassword(
+                        //               email: UserController.text,
+                        //               password: passwordController.text
+                        //           );
+                        //           Navigator.pushReplacement(context,
+                        //               MaterialPageRoute(
+                        //                   builder: (context) => HomeScreen())
+                        //           );
+                        //         } on FirebaseAuthException catch (e) {
+                        //           if (e.code == 'user-not-found') {
+                        //             AwesomeDialog(
+                        //               context: context,
+                        //               dialogType: DialogType.error,
+                        //               animType: AnimType.rightSlide,
+                        //               title: ' خطاء',
+                        //               desc: 'لم يتم العثور علي اسم مستخدم',
+                        //             ).show();
+                        //             print(
+                        //                 '=====لم يتم العثور علي مستخدم لهذ الايميل======');
+                        //
+                        //           } else if (e.code == 'wrong-password') {
+                        //             AwesomeDialog(
+                        //               context: context,
+                        //               dialogType: DialogType.error,
+                        //               animType: AnimType.rightSlide,
+                        //               title: ' خطاء',
+                        //               desc: 'كلمه المرور غير صحيحه',
+                        //             ).show();
+                        //             print('======كلمه المرور غير صحيحه=======');
+                        //           }
+                        //         }
+                        //       }
+                        //     } ,
+                        //     text: 'تسجيل دخول'
+                        //
+                        // ),
+                        SizedBox(height: 20.0,),
+                        Center(
+                          child: MaterialButton(
+                            onPressed: (){
+                              signInWithGoogle();
+                            },child:Image.asset(
+                            'assets/imdegoogle.png',
+                            height: 30.0,
+                            width: 30,
+                          ) ,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(onPressed: (){
+                              navigatorTo(context, register());
+                            }, child: Text('انشاء حساب'),
+                            ),
+                          ],
+                        )
+
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ) ,
+        );
+        },
+
+      ),
     );
   }
 }
